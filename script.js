b00c32c8724cc1218537df95ab4cf0a83538047e
Dixant <139498534+broskidix@users.noreply.github.com>
2023-07-15 16:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ arr.forEach(button => {
             string = "";
             input.value = string;
         }
-        else if(e.target.innerHTML == 'DELETE'){
+        else if(e.target.innerHTML == 'DEL'){
             string = string.substring(0, string.length-1);
             input.value = string;
         }
